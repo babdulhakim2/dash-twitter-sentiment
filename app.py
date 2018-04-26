9253c04d5e52fdee80fc2131e773b58bd5a17571
Abdul <you@example.com>
2018-04-26 09:08:18 +0100

    res

--- app.py
+++ app.py
@@ -1,29 +1,62 @@
-import os
-
 import dash
+from dash.dependencies import Output, Event, Input
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+import sqlite3
+import pandas as pd 
 
+#popular topics: google, olympics, trump, gun, usa
 
 app = dash.Dash(__name__)
 server = app.server
+app.layout = html.Div(
+    [   html.H2('Live Twitter Sentiment'),
+    dcc.Input(id='sentiment_term', value='twitter', type='text'),
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1*1000
+        ),
+    ]
+)
+@app.callback(Output('live-graph', 'figure'),
+    [Input(component_id='sentiment_term', component_property='value')],
+              events=[Event('graph-update', 'interval')])
+def update_graph_scatter(sentiment_term):
+    try:
+        conn = sqlite3.connect('nigeria_tweeps.db')
+        c = conn.cursor()
+        df = pd.read_sql("SELECT * FROM sentiment WHERE tweet LIKE ? ORDER BY unix DESC LIMIT 1000", conn ,params=('%' + sentiment_term + '%',))
+        df.sort_values('unix', inplace=True)
+        df["date"] = pd.to_datetime(df['unix'], unit='ms')
+        df.set_index('date', inplace=True)
+        df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
+        df.dropna(inplace=True)
+        # df = df.resample('1s').mean()
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+        X = df.index[-100:]
+        Y = df.sentiment_smoothed.values[-100:]
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
+        data = plotly.graph_objs.Scatter(
+                x=X,
+                y=Y,
+                name='Scatter',
+                mode= 'lines+markers'
+                )
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+        return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                    yaxis=dict(range=[min(Y),max(Y)]),
+                                                    title="Term: {}".format(sentiment_term))}
+
+    except Exception as e:
+        with open('errors.txt','a') as f:
+            f.write(str(e))
+            f.write('\n')
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
